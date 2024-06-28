36369602afc5ef41285876be72ea524a9dc96123
Justin Tanuwijaya <justintnwj2@gmail.com>
2024-06-28 20:30:44 +0700

    tsc-commit

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,28 +1,28 @@
 const config = {
-    DB_USERNAME: "postgres",
-    DB_PASSWORD: "123456",
-    DB_HOST: "127.0.0.1",
-    DB_NAME: "database",
+    DB_USERNAME: "postgres.qpbapqnqlbjtavgwkgsi",
+    DB_PASSWORD: "binar-car-rental123",
+    DB_HOST: "aws-0-ap-southeast-1.pooler.supabase.com",
+    DB_NAME: "postgres",
 };
 module.exports = {
     development: {
         username: config.DB_USERNAME,
         password: config.DB_PASSWORD,
-        database: `${config.DB_NAME}_development`,
+        database: `${config.DB_NAME}`,
         host: config.DB_HOST,
         dialect: "postgres",
     },
     test: {
         username: config.DB_USERNAME,
         password: config.DB_PASSWORD,
-        database: `${config.DB_NAME}_test`,
+        database: `${config.DB_NAME}`,
         host: config.DB_HOST,
         dialect: "postgres",
     },
     production: {
         username: config.DB_USERNAME,
         password: config.DB_PASSWORD,
-        database: `${config.DB_NAME}_production`,
+        database: `${config.DB_NAME}`,
         host: config.DB_HOST,
         dialect: "postgres",
     },
